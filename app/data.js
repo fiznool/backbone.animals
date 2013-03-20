47ad0f88adc6572388c4b68ef13301a9ab6a4122
fiznool <fiznool@gmail.com>
2013-03-20 13:40:45 +0000

    Start of parse.com integration

--- app/data.js
+++ app/data.js
@@ -3,8 +3,24 @@ define([
 ],
 
 function(Backbone) {
-    var urlRoot = 'api/';
+    //var urlRoot = 'api/';
+    var apiKey = 'Q14WG6VbwprFAQn7JcaMAytxFjmw9dyXrzimg87c';
+    var restKey = '13raxBKBoUbIHgNoOsRmMu9oV34MUDRk0LUFEw7c';
+    var urlRoot = 'https://api.parse.com/1/classes/';
     var imgRoot = 'api/images/';
+
+    var originalSync = Backbone.sync;
+
+    // Override Backbone.sync for all future requests,
+    // setting the Parse headers before the sync is performed.
+    Backbone.sync = function(method, model, options) {
+        options.headers = options.headers || {};
+        _.extend(options.headers, {
+            'X-Parse-Application-Id': apiKey,
+            'X-Parse-REST-API-Key': restKey
+        });
+        return originalSync.call(model, method, model, options);
+    };
 
     var Data = {
         Headerbar: {},
@@ -25,6 +41,7 @@ function(Backbone) {
         },
 
         parse: function(resp) {
+            resp.id = resp.objectId;
             resp.img = imgRoot + resp.img;
             return resp;
         }
@@ -32,7 +49,10 @@ function(Backbone) {
 
     Data.Animals.Collection = Backbone.Collection.extend({
         model: Data.Animals.Model,
-        url: urlRoot + 'animals'
+        url: urlRoot + 'animals',
+        parse: function(resp) {
+            return resp && resp.results;
+        }
     });
 
     return Data;
